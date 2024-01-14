d43a5cc80fe1eb2cd87bb9a4d45f649fe85c2c93
dianlucky <dianlucky13@gmail.com>
2024-01-14 20:11:34 +0800

    [Service] Update

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,28 +8,31 @@ export class ApiService {
   constructor(public http: HttpClient) {}
 
   addPegawai(data: any) {
-    return this.http.post('http://simpatik.id/kantor/create_pegawai.php', data);
+    return this.http.post(
+      'https://simpatik.id/kantor/create_pegawai.php',
+      data
+    );
   }
 
   getPegawai() {
-    return this.http.get('http://simpatik.id/kantor/get_all_pegawai.php');
+    return this.http.get('https://simpatik.id/kantor/get_all_pegawai.php');
   }
 
   deletePegawai(id: any) {
     return this.http.get(
-      'http://simpatik.id/kantor/delete_pegawai.php?id=' + id
+      'https://simpatik.id/kantor/delete_pegawai.php?id=' + id
     );
   }
 
   getOnePegawai(id: any) {
     return this.http.get(
-      'http://simpatik.id/kantor/get_one_pegawai.php?id=' + id
+      'https://simpatik.id/kantor/get_one_pegawai.php?id=' + id
     );
   }
 
   updatePegawai(id: any, data: any) {
     return this.http.put(
-      'http://simpatik.id/kantor/update_pegawai.php?id=' + id,
+      'https://simpatik.id/kantor/update_pegawai.php?id=' + id,
       data
     );
   }
